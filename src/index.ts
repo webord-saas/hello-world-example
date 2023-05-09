e231a6344d1f63b108569220276537f43eefc95d
Henning Norden <heno@henomedia.de>
2023-05-10 00:38:56 +0200

    🔨 refactor(index.d.ts, index.js, index.ts): Ändern der HelloWorldExamplePlugin-Klasse in ein Objekt mit einer Webord-Eigenschaft und einer init-Methode.

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,27 @@
 import {Webord} from '../node_modules/webord-plugin/lib/index';
 import {HelloWorld} from './screens/HelloWorld';
 
-class HelloWorldExamplePlugin {
-	WebordVal: typeof Webord = window.Webord || Webord;
+declare const window: any;
+let HelloWorldExamplePlugin = {
+	Webord: <typeof Webord>window.Webord || Webord,
 
-	init = () => {
-		if (!this.WebordVal) {
-			throw new Error('Webord is not defined');
-		}
-
-		this.WebordVal.registerCategory({
+	init: () => {
+		Webord.registerCategory({
 			key: 'hello-world',
 			name: 'Hello World from Plugin',
 			path: '/hello-world',
 		});
 
-		this.WebordVal.registerLink({
+		Webord.registerLink({
 			key: 'hello-world',
 			name: 'Hello World',
 			path: '/',
 			categoryKey: 'hello-world',
 			component: HelloWorld,
 		});
-	};
-}
-declare const window: any;
+	},
+};
+
 if (typeof window !== 'undefined') {
 	window.HelloWorldExamplePlugin = HelloWorldExamplePlugin;
 }
