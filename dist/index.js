216063c00c1f34d7624b8a618e3d23873d06f95a
Henning Norden <heno@henomedia.de>
2023-05-10 16:43:48 +0200

    🔨 refactor(index.js): Refaktorisiere den Code, um die Lesbarkeit zu verbessern.

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,32 @@
-!function(e,o){"object"==typeof exports&&"object"==typeof module?module.exports=o():"function"==typeof define&&define.amd?define("HelloWorldExample",[],o):"object"==typeof exports?exports.HelloWorldExample=o():e.HelloWorldExample=o()}(self,(()=>(()=>{"use strict";let e=window.WebordPlugin;return e.registerCategory({key:"hello-world",name:"Hello World from Plugin",path:"/hello-world"}),e.registerLink({key:"hello-world",name:"Hello World",path:"/",categoryKey:"hello-world",component:()=>document.createElement("p").innerHTML="Hello World from Plugin"}),console.log("Hello World from Plugin"),{}})()));
+!(function (e, o) {
+	'object' == typeof exports && 'object' == typeof module
+		? (module.exports = o())
+		: 'function' == typeof define && define.amd
+		? define('HelloWorldExample', [], o)
+		: 'object' == typeof exports
+		? (exports.HelloWorldExample = o())
+		: (e.HelloWorldExample = o());
+})(self, () =>
+	(() => {
+		'use strict';
+		let e = window.WebordPlugin;
+		return (
+			e.registerCategory({
+				key: 'hello-world',
+				name: 'Hello World from Plugin',
+				path: '/hello-world',
+			}),
+			e.registerLink({
+				key: 'hello-world',
+				name: 'Hello World',
+				path: '/',
+				categoryKey: 'hello-world',
+				component: () =>
+					(document.createElement('p').innerHTML = 'Hello World from Plugin'),
+			}),
+			console.log('Hello World from Plugin'),
+			{}
+		);
+	})()
+);
 //# sourceMappingURL=index.js.map
